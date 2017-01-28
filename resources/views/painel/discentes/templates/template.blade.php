f4deb0ee0d537f91d82a45bcc1ce8d7583bc1c15
Luis-Souza <jorgeluis0796@gmail.com>
2017-01-28 17:10:40 -0200

    modificando templates de discente, add arquivos de layout em painel\sources

--- resources/views/painel/discentes/templates/template.blade.php
+++ resources/views/painel/discentes/templates/template.blade.php
@@ -3,72 +3,162 @@
     <head>
         <title>{{$title or 'Cadastro-Edite'}}</title>
         <meta charset="utf-8">
+
         <link rel="stylesheet" href="{{url('bootstrap/css/bootstrap.min.css')}}">   
-        <link href="{{url('sources/painel/css/style-admin.css')}}" rel="stylesheet">
+
+
+        <!-- MetisMenu CSS -->
+        <link href="{{url('sources/painel/css/metisMenu.min.css')}}" rel="stylesheet">
+
+        <!-- Timeline CSS -->
+        <link href="{{url('sources/painel/css/timeline.css')}}" rel="stylesheet">
+
+        <!-- Custom CSS -->
+        <link href="{{url('sources/painel/css/startmin.css')}}" rel="stylesheet">
+
+        <!-- Morris Charts CSS -->
+        <link href="{{url('sources/painel/css/morris.css')}}" rel="stylesheet">
 
     </head> 
     <body>
-        <div class="container">
-            <!-- NAV SECTION -->
-            <div class="navbar navbar-inverse navbar-fixed-top">
-                <div class="container">
-                    <div class="navbar-header">
-                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                        </button>
-                        <!-- Branding Image -->
-                        <a class="navbar-brand" href="{{ url('/') }}">
-                            {{ config('app.name', 'Sistema CEE') }}
+
+        <div id="wrapper">
+
+            <!-- Navigation -->
+            <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="#">Sistema CEE</a>
+                </div>
+
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+
+                <!-- Top Navigation: Left Menu -->
+                <ul class="nav navbar-nav navbar-left navbar-top-links">
+                    <li><a href="#"><i class="glyphicon glyphicon-home"></i> Website</a></li>
+                </ul>
+
+                <!-- Top Navigation: Right Menu -->
+                <ul class="nav navbar-right navbar-top-links">
+                    <li class="dropdown navbar-inverse">
+                        <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                            <i class="glyphicon glyphicon-bell"></i> <b class="caret"></b>
                         </a>
-                    </div>
-                    <div class="navbar-collapse collapse">
-                        <ul class="nav navbar-nav navbar-right">
-                            <li><a href="#home-sec">HOME</a></li>
-                            <li><a href="#services-sec">SERVICES</a></li>
-                            <li><a href="#contact-sec">CONTACT</a></li>
-
-                            <!-- mudei o login Pra ka!!!! -->
-                            @if (Auth::guest())
-                            <li><a href="{{ url('/login') }}">LOGIN</a></li>
-                            <li><a href="{{ url('/register') }}">REGISTRE-SE</a></li>
-                            @else
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                    {{ Auth::user()->name }} <span class="caret"></span>
+                        <ul class="dropdown-menu dropdown-alerts">
+                            <li>
+                                <a href="#">
+                                    <div>
+                                        <i class="fa fa-comment fa-fw"></i> New Comment
+                                        <span class="pull-right text-muted small">4 minutes ago</span>
+                                    </div>
                                 </a>
-
-                                <ul class="dropdown-menu" role="menu">
-                                    <li>
-                                        <a href="{{ url('/logout') }}"
-                                           onclick="event.preventDefault();
-                                                   document.getElementById('logout-form').submit();">
-                                            Logout
-                                        </a>
-
-                                        <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-                                </ul>
                             </li>
-                            @endif 
+                            <li class="divider"></li>
+                            <li>
+                                <a class="text-center" href="#">
+                                    <strong>See All Alerts</strong>
+                                    <i class="fa fa-angle-right"></i>
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                    <!-- mudei o login Pra ka!!!! -->
+                    @if (Auth::guest())
+                    <li><a href="{{ url('/login') }}">LOGIN</a></li>
+                    <li><a href="{{ url('/register') }}">REGISTRE-SE</a></li>
+                    @else
+                       <li class="dropdown">
+                       <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                       
+                             <span class="glyphicon glyphicon-user"></span>  {{ Auth::user()->name }} <span class="caret"></span>
+                       </a>
 
+                       <ul class="dropdown-menu" role="menu">
+                       <li>
+                                <!-- link para o historico de estagios -->
+                            <a href="#" class="active"><i class="glyphicon glyphicon-user"></i> &nbsp; Perfil</a>
+                       </li>
+                       <li class="divider"></li>
+                       <li>
+                            <a href="{{ url('/logout') }}"
+                                       onclick="event.preventDefault();
+                                               document.getElementById('logout-form').submit();">
+                                         <i class="glyphicon glyphicon-log-in"></i> &nbsp; Sair  
+                            </a>
+
+                               <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
+                                        {{ csrf_field() }}
+                               </form>
+                        </li>
+                        
+                        </ul>
+                        </li>
+                     @endif 
+
+                </ul>
+
+                <!-- Sidebar -->
+                <div class="navbar-default sidebar" role="navigation">
+                    <div class="sidebar-nav navbar-collapse">
+
+                        <ul class="nav" id="side-menu">                    
+                            <li>
+                                <!-- link para o historico de estagios -->
+                                <a href="#" class="active"><i class="glyphicon glyphicon-dashboard"></i> Historico</a>
+                            </li>
+                            <li>
+                                <!-- link para o historico de estagios -->
+                                <a href="#" class="active"><i class="glyphicon glyphicon-eye-open"></i> Curriculo</a>
+                            </li>
                         </ul>
 
                     </div>
+                </div>
+            </nav>
+
+            <!-- Page Content -->
+            <div id="page-wrapper">
+                <div class="container-fluid">
+
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <h1 class="page-header">{{$title or "Dados"}}</h1>
+
+                        </div>
+                    </div>
+                    </br>
+                    <div class="col-md-8 col-md-offset-2">
+                    @yield('content')
+                        
+
+                    </div>
+                    <!-- ... Your content goes here ... -->
 
                 </div>
+                    </br>
+                    </br>
             </div>
-            <!--END NAV SECTION -->
-            @yield('content')
         </div>
+
+
     </body>
+
     <!-- jQuery -->
-    <script src="{{url('bootstrap/js/jquery.js')}}"></script>
+    <!-- <script src="{{url('bootstrap/js/jquery.js')}}"></script> -->
+    <script src="{{url('sources/painel/js/jquery.min.js')}}"></script>
 
     <!-- Bootstrap Core JavaScript -->
-    <script src="{{url('bootstrap/js/bootstrap.min.js')}}"></script>
+<!--     <script src="{{url('bootstrap/js/bootstrap.min.js')}}"></script> -->
+    <script src="{{url('sources/painel/js/bootstrap.min.js')}}"></script>
+
+    <!-- Metis Menu Plugin JavaScript -->
+    <script src="{{url('sources/painel/js/metisMenu.min.js')}}"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="{{url('sources/painel/js/startmin.js')}}"></script>
 
 </html>
